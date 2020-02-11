fc290892358b00050b1b5c2ccb5f71c550393be0
morinoko <forby.1@gmail.com>
2020-02-11 17:07:43 +0900

    set javascript for qr-reader that makes a post request to rails

---
+++ app/javascript/packs/rails-qr-code-reader.js
@@ -0,0 +1,31 @@
+import { BrowserQRCodeReader } from '@zxing/library';
+import Rails from '@rails/ujs'; // Use to make an ajax post request to Rails
+
+const codeReader = new BrowserQRCodeReader();
+
+codeReader
+  .decodeFromInputVideoDevice(undefined, 'video')
+  .then((result) => {
+    let qrDataFromReader = result.text;
+
+    // Prepare a post request so it can be sent to the Rails controller
+    let formData = new FormData();
+
+    let qrCodeParams = {
+      qr_data: qrDataFromReader
+    };
+
+    formData.append("qr_code_json_data", JSON.stringify(qrCodeParams));
+
+    // Send QR code data as JSON to the 
+    // qr_codes#create action using Rails UJS
+    Rails.ajax({
+      url: "/rails_qr_codes",
+      type: "post",
+      data: formData
+    });
+
+  })
+  .catch(error => {
+    console.error(error);
+  });
