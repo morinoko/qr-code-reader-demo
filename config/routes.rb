f0570782c4c95cc9b89a27208eb04924f9a62ed2
morinoko <forby.1@gmail.com>
2020-02-11 11:42:44 +0900

    fix comment

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within t
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
   get 'basic-qr-code-reader', to: 'basic_qr_codes#new'
 end
