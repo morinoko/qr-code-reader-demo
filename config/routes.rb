cd19799670c47692275656f849f923df18e916a3
morinoko <forby.1@gmail.com>
2020-02-11 17:22:45 +0900

    make home page with links to different demos

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
+  root to: 'home#index'
+  
   get 'basic-qr-code-reader', to: 'basic_qr_codes#new'
   #get 'qr-code-reader-with-rails', to: 'rails_qr_codes#new'
-  resources :rails_qr_codes, only: [:new, :create, :show]
+  resources :rails_qr_codes, only: [:index, :new, :create, :show]
 end
