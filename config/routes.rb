d0be803c4f88104ce13a6e3d3dff22ac5bc17db6
morinoko <forby.1@gmail.com>
2020-02-11 17:08:09 +0900

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  
+
   get 'basic-qr-code-reader', to: 'basic_qr_codes#new'
+  #get 'qr-code-reader-with-rails', to: 'rails_qr_codes#new'
+  resources :rails_qr_codes, only: [:new, :create, :show]
 end
