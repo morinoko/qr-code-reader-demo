75da7e1882ddd30f94893a9342f3452b985edeb5
morinoko <forby.1@gmail.com>
2020-02-17 08:34:33 +0900

    rename action to index

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'home#index'
   
-  get 'basic-qr-code-reader', to: 'basic_qr_codes#new'
+  get 'basic-qr-code-reader', to: 'basic_qr_codes#index'
   resources :rails_qr_codes, only: [:index, :new, :create, :show]
 end
